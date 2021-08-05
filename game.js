a9e201418442fc9c72a2a714084aa6aae14db545
GalaxyGayboi <galaxygayboi@gmail.com>
2021-08-04 20:15:22 -0500

    One game version that never changes computerSelection

--- game.js
+++ game.js
@@ -1,9 +1,43 @@
 // create player action variable (playerSelection)
-// create cpu action variable (computerSelection)
+let playerSelection = "placeholder";
 // ask user for their choice
-// randomize cpuChoice and save it as a var
+// define rock paper and scissors
+let rock = "Rock"
+let paper = "Paper"
+let scissors = "Scissors"
+// randomize cpuChoice
+function computerPlay() {
+    randomNum = Math.floor(Math.random()* 3) + 1;
+    if (randomNum === 1) {
+        return rock;
+    } else if (randomNum === 2) {
+        return paper;
+    } else {
+        return scissors;
+    }
+}
+// create cpu action variable (computerSelection)
+let computerSelection = computerPlay();
 // function that pits userChoice against random cpuChoice (playRound)
-// rock beats scissors
-// scissors beats paper
-// paper beats rock
+function playRound(playerSelection) {
+    let newplayerSelection = playerSelection.toLowerCase();
+    // rock beats scissors
+    console.log(computerSelection)
+    if (newplayerSelection === "rock" && computerSelection === scissors) {
+        return "You win! Rock beats scissors."
+    // paper beats rock
+    } else if (newplayerSelection === "paper" && computerSelection === rock) {
+        return "You win! Paper beats rock."
+    // scissors beats paper
+    } else if (newplayerSelection === "scissors" && computerSelection === paper) {
+        return "You win! Scissors beats paper."
+    } else if (newplayerSelection === computerSelection) {
+        return `You tied. Derek's ${computerSelection} is the same as your ${newplayerSelection}.`
+    } else {
+        return `You lost... ${computerSelection} beats ${newplayerSelection}.`
+    }
+}
+
+
+// create 5 game format 
 // show user the result in a string i.e (you win, you lose playerSelection beats computerSelection)
