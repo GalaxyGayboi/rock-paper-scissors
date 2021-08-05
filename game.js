cd9a12620a0413a11e31618cedf4f46eff711718
GalaxyGayboi <galaxygayboi@gmail.com>
2021-08-04 19:13:38 -0500

    Added Psuedocode for game 1

---
+++ game.js
@@ -0,0 +1,9 @@
+// create player action variable (playerSelection)
+// create cpu action variable (computerSelection)
+// ask user for their choice
+// randomize cpuChoice and save it as a var
+// function that pits userChoice against random cpuChoice (playRound)
+// rock beats scissors
+// scissors beats paper
+// paper beats rock
+// show user the result in a string i.e (you win, you lose playerSelection beats computerSelection)
